78a8c57c6e307f4d66eb9e1c20052af451187299
Seamoon Pandey <hackedasocial@gmail.com>
2022-11-09 10:44:34 +0545

    some minor changes with the bootstrap

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './NavBar.css'
 import swal from 'sweetalert'
 import { useHistory } from 'react-router-dom'
